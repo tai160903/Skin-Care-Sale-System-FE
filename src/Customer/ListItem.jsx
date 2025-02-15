9b0e33c6f3ea10885c1e358fee443bf787e697d2
MACOS <lethinhphat7102003@gmail.com>
2025-02-15 14:54:44 +0700

    Cập nhật code mới

---
+++ src/Customer/ListItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { List, ListItem, ListItemText, Paper } from "@mui/material";
+
+const ListItems = ({ items, onItemClick }) => {
+  return (
+    <Paper elevation={3} sx={{ maxWidth: 400, margin: "auto", padding: 2 }}>
+      <List>
+        {items.map((item, index) => (
+          <ListItem 
+            key={index} 
+            button 
+            onClick={() => onItemClick && onItemClick(item)}
+          >
+            <ListItemText primary={item} />
+          </ListItem>
+        ))}
+      </List>
+    </Paper>
+  );
+};
+
+export default ListItems;
